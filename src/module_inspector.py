cc6653e610ec0a58ad89e8c37653d2bb55bfde77
gazorpazorpius <edcussmusic@gmail.com>
2024-03-16 17:35:51 +0000

    parametrized tests

--- src/module_inspector.py
+++ src/module_inspector.py
@@ -52,8 +52,25 @@ class ModuleInspector:
             k: v for k, v in self._module_vars.items() if inspect.isclass(v)
         }
 
-    def print_function_tests(self):
-        for k,v in self.custom_functions_.items():
-            sig = str(inspect.signature(v)).split(") ->")[0]
-            print(f"def test_{k}{sig}, expected_result) -> None:\n    assert {k}{sig}) == \n\n")
+    def get_params_str(self, func):
+        return ", ".join(list(inspect.signature(func).parameters.keys()))
 
+    def get_parametrize_decorator(self, func):
+        args = self.get_params_str(func)
+        return f'@pytest.mark.parametrize(\n    "{args}, expected_result",\n    [\n        ({args}, expected_result),\n    ]\n)'
+
+    def get_function_test(self, func_name, func):
+        sig = self.get_params_str(func)
+        return f"def test_{func_name}({sig}, expected_result) -> None:\n    assert {func_name}({sig}) == expected_result\n\n"
+
+    def print_function_tests(self):
+        for k, v in self.custom_functions_.items():
+            sig = self.get_params_str(v)
+            print(
+                f"def test_{k}({sig}, expected_result) -> None:\n    assert {k}({sig}) == expected_result\n\n"
+            )
+
+    def print_parametrized_function_tests(self):
+        for k, v in self.custom_functions_.items():
+            print(self.get_parametrize_decorator(v))
+            print(self.get_function_test(k, v))
