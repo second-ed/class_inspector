6730b5ab06f881d50d2567859a6abd274df95340
gazorpazorpius <edcussmusic@gmail.com>
2024-05-27 20:08:44 +0100

    cleanup: reorder functions to be more clear

--- src/class_inspector/enforce_types.py
+++ src/class_inspector/enforce_types.py
@@ -2,33 +2,6 @@ import inspect
 from typing import Any, Callable, Union, get_args, get_origin
 
 
-def simplify_annotation(annotation: type):
-    origin = get_origin(annotation)
-    args = get_args(annotation)
-
-    # is native or custom type
-    if origin is None:
-        return annotation
-
-    if origin is Union:
-        # is Optional type
-        if type(None) in args:
-            non_none_args = [arg for arg in args if arg is not type(None)]
-            if len(non_none_args) == 1:
-                return (simplify_annotation(non_none_args[0]), type(None))
-        return tuple(simplify_annotation(arg) for arg in args)
-    return origin
-
-
-def check_type(arg: Any, param_name: str, param_type: type) -> str:
-    if not isinstance(arg, simplify_annotation(param_type)):
-        return (
-            f"\n    arg {param_name} expects type: {param_type.__name__}, "
-            f"recieved: {type(arg).__name__}"
-        )
-    return ""
-
-
 def enforce_types(func: Callable) -> Callable:
     def wrapper(*args, **kwargs):
         signature = inspect.signature(func)
@@ -52,3 +25,30 @@ def enforce_types(func: Callable) -> Callable:
         return func(*args, **kwargs)
 
     return wrapper
+
+
+def check_type(arg: Any, param_name: str, param_type: type) -> str:
+    if not isinstance(arg, simplify_annotation(param_type)):
+        return (
+            f"\n    arg {param_name} expects type: {param_type.__name__}, "
+            f"recieved: {type(arg).__name__}"
+        )
+    return ""
+
+
+def simplify_annotation(annotation: type):
+    origin = get_origin(annotation)
+    args = get_args(annotation)
+
+    # is native or custom type
+    if origin is None:
+        return annotation
+
+    if origin is Union:
+        # is Optional type
+        if type(None) in args:
+            non_none_args = [arg for arg in args if arg is not type(None)]
+            if len(non_none_args) == 1:
+                return (simplify_annotation(non_none_args[0]), type(None))
+        return tuple(simplify_annotation(arg) for arg in args)
+    return origin
