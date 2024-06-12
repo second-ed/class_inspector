d95c2ef87d53cf27b07f0283af77fd0b1b7d5a62
gazorpazorpius <edcussmusic@gmail.com>
2024-06-12 23:32:23 +0100

    tests: edit tests to accept parametrized fixtures

--- tests/test_function_inspector.py
+++ tests/test_function_inspector.py
@@ -1,11 +1,16 @@
 from contextlib import nullcontext as does_not_raise
+from typing import Callable
 
 import pytest
 from class_inspector.function_inspector import FunctionInspector
 
 
-@pytest.fixture
-def test_function(
+class MockClass:
+    def mock_method(self, a: int, b: str) -> str:
+        return str(a) + b
+
+
+def mock_function(
     param1: float, param2: int, param3: bool, param4: str = "test"
 ) -> float:
     if param3:
@@ -15,46 +20,80 @@ def test_function(
 
 
 @pytest.fixture
+def get_mock_function() -> Callable:
+    return mock_function
+
+
+@pytest.fixture
+def get_mock_method() -> Callable:
+    return MockClass().mock_method
+
+
+@pytest.fixture
 def get_instance() -> FunctionInspector:
     return FunctionInspector()
 
 
-def test_analyse(get_instance: FunctionInspector) -> None:
-    get_instance.analyse(test_function)
-    assert get_instance.obj == test_function
-    assert get_instance.name == "test_function"
-    assert get_instance.parameters == {
-        "param1": float,
-        "param2": int,
-        "param3": bool,
-        "param4": str,
-    }
-    assert get_instance.return_annotation == "float"
+@pytest.mark.parametrize(
+    "fixture_name, func_name, params, return_annot",
+    [
+        (
+            "get_mock_function",
+            "mock_function",
+            {
+                "param1": float,
+                "param2": int,
+                "param3": bool,
+                "param4": str,
+            },
+            "float",
+        )
+    ],
+)
+def test_analyse(
+    request,
+    get_instance: FunctionInspector,
+    fixture_name,
+    func_name,
+    params,
+    return_annot,
+) -> None:
+    func = request.getfixturevalue(fixture_name)
+    get_instance.analyse(func)
+    assert get_instance.obj == func
+    assert get_instance.name == func_name
+    assert get_instance.parameters == params
+    assert get_instance.return_annotation == return_annot
 
 
 @pytest.mark.parametrize(
-    "func, expected_result, expected_context",
+    "fixture_name, expected_result, expected_context",
     [
-        (test_function, "test_function", does_not_raise()),
+        ("get_mock_function", "mock_function", does_not_raise()),
     ],
 )
 def test_get_class_name(
-    get_instance: FunctionInspector, func, expected_result, expected_context
+    request,
+    get_instance: FunctionInspector,
+    fixture_name,
+    expected_result,
+    expected_context,
 ) -> None:
     with expected_context:
+        func = request.getfixturevalue(fixture_name)
         get_instance.analyse(func)
         assert get_instance._get_class_name() == expected_result
 
 
 @pytest.mark.parametrize(
-    "func, expected_result, expected_context",
+    "fixture_name, expected_result, expected_context",
     [
         (
-            test_function,
+            "get_mock_function",
             (
                 "    if not all([isinstance(param1, float), isinstance(param2, int), isinstance(param3, bool), isinstance(param4, str)]):\n"
                 "        raise TypeError(\n"
-                '            "test_function expects arg types: [float, int, bool, str], "\n'
+                '            "mock_function expects arg types: [float, int, bool, str], "\n'
                 '            f"received: [{type(param1).__name__}, {type(param2).__name__}, {type(param3).__name__}, {type(param4).__name__}]"\n'
                 "        )\n"
             ),
@@ -63,50 +102,65 @@ def test_get_class_name(
     ],
 )
 def test_get_guards(
-    get_instance: FunctionInspector, func, expected_result, expected_context
+    request,
+    get_instance: FunctionInspector,
+    fixture_name,
+    expected_result,
+    expected_context,
 ) -> None:
     with expected_context:
+        func = request.getfixturevalue(fixture_name)
         get_instance.analyse(func)
         assert get_instance._get_guards() == expected_result
 
 
 @pytest.mark.parametrize(
-    "func, expected_result, expected_context",
+    "fixture_name, expected_result, expected_context",
     [
         (
-            test_function,
-            "test_function(param1, param2, param3, param4) ",
+            "get_mock_function",
+            "mock_function(param1, param2, param3, param4) ",
             does_not_raise(),
         ),
     ],
 )
 def test_get_instance_call(
-    get_instance: FunctionInspector, func, expected_result, expected_context
+    request,
+    get_instance: FunctionInspector,
+    fixture_name,
+    expected_result,
+    expected_context,
 ) -> None:
     with expected_context:
+        func = request.getfixturevalue(fixture_name)
         get_instance.analyse(func)
         assert get_instance._get_instance_call() == expected_result
 
 
 @pytest.mark.parametrize(
-    "func, expected_result, expected_context",
+    "fixture_name, expected_result, expected_context",
     [
-        (test_function, "", does_not_raise()),
+        ("get_mock_function", "", does_not_raise()),
     ],
 )
 def test_get_instance_sig(
-    get_instance: FunctionInspector, func, expected_result, expected_context
+    request,
+    get_instance: FunctionInspector,
+    fixture_name,
+    expected_result,
+    expected_context,
 ) -> None:
     with expected_context:
+        func = request.getfixturevalue(fixture_name)
         get_instance.analyse(func)
         assert get_instance._get_instance_sig() == expected_result
 
 
 @pytest.mark.parametrize(
-    "func, check_types, match, expected_result, expected_context",
+    "fixture_name, check_types, match, expected_result, expected_context",
     [
         (
-            test_function,
+            "get_mock_function",
             True,
             False,
             (
@@ -123,7 +177,7 @@ def test_get_instance_sig(
             does_not_raise(),
         ),
         (
-            test_function,
+            "get_mock_function",
             True,
             True,
             (
@@ -140,7 +194,7 @@ def test_get_instance_sig(
             does_not_raise(),
         ),
         (
-            test_function,
+            "get_mock_function",
             False,
             True,
             (
@@ -155,14 +209,16 @@ def test_get_instance_sig(
     ],
 )
 def test_get_parametrize_decorator(
+    request,
     get_instance: FunctionInspector,
-    func,
+    fixture_name,
     check_types,
     match,
     expected_result,
     expected_context,
 ) -> None:
     with expected_context:
+        func = request.getfixturevalue(fixture_name)
         get_instance.analyse(func)
         assert (
             get_instance._get_parametrize_decorator(check_types, match)
@@ -171,52 +227,71 @@ def test_get_parametrize_decorator(
 
 
 @pytest.mark.parametrize(
-    "func, expected_result, expected_context",
+    "fixture_name, expected_result, expected_context",
     [
-        (test_function, "param1, param2, param3, param4", does_not_raise()),
+        (
+            "get_mock_function",
+            "param1, param2, param3, param4",
+            does_not_raise(),
+        ),
     ],
 )
 def test_get_params_str(
-    get_instance: FunctionInspector, func, expected_result, expected_context
+    request,
+    get_instance: FunctionInspector,
+    fixture_name,
+    expected_result,
+    expected_context,
 ) -> None:
     with expected_context:
+        func = request.getfixturevalue(fixture_name)
         get_instance.analyse(func)
         assert get_instance._get_params_str() == expected_result
 
 
 @pytest.mark.parametrize(
-    "func, expected_result, expected_context",
+    "fixture_name, expected_result, expected_context",
     [
-        (test_function, "float, int, bool, str", does_not_raise()),
+        ("get_mock_function", "float, int, bool, str", does_not_raise()),
     ],
 )
 def test_get_params_types(
-    get_instance: FunctionInspector, func, expected_result, expected_context
+    request,
+    get_instance: FunctionInspector,
+    fixture_name,
+    expected_result,
+    expected_context,
 ) -> None:
     with expected_context:
+        func = request.getfixturevalue(fixture_name)
         get_instance.analyse(func)
         assert get_instance._get_params_types() == expected_result
 
 
 @pytest.mark.parametrize(
-    "func, expected_result, expected_context",
+    "fixture_name, expected_result, expected_context",
     [
-        (test_function, "float", does_not_raise()),
+        ("get_mock_function", "float", does_not_raise()),
     ],
 )
 def test_get_return_annotations(
-    get_instance: FunctionInspector, func, expected_result, expected_context
+    request,
+    get_instance: FunctionInspector,
+    fixture_name,
+    expected_result,
+    expected_context,
 ) -> None:
     with expected_context:
+        func = request.getfixturevalue(fixture_name)
         get_instance.analyse(func)
         assert get_instance._get_return_annotations() == expected_result
 
 
 @pytest.mark.parametrize(
-    "func, check_types, match, expected_result, expected_context",
+    "fixture_name, check_types, match, expected_result, expected_context",
     [
         (
-            test_function,
+            "get_mock_function",
             True,
             False,
             (
@@ -229,14 +304,14 @@ def test_get_return_annotations(
                 "        (param1, param2, param3, param4, None, pytest.raises(TypeError)),\n"
                 "        (param1, param2, param3, param4, None, pytest.raises(TypeError)),\n"
                 "    ]\n)\n"
-                "def test_test_function(param1, param2, param3, param4, expected_result, expected_context) -> None:\n"
+                "def test_mock_function(param1, param2, param3, param4, expected_result, expected_context) -> None:\n"
                 "    with expected_context:\n"
-                "        assert test_function(param1, param2, param3, param4) == expected_result\n\n\n"
+                "        assert mock_function(param1, param2, param3, param4) == expected_result\n\n\n"
             ),
             does_not_raise(),
         ),
         (
-            test_function,
+            "get_mock_function",
             True,
             True,
             (
@@ -249,14 +324,14 @@ def test_get_return_annotations(
                 '        (param1, param2, param3, param4, None, pytest.raises(TypeError, match=r"")),\n'
                 '        (param1, param2, param3, param4, None, pytest.raises(TypeError, match=r"")),\n'
                 "    ]\n)\n"
-                "def test_test_function(param1, param2, param3, param4, expected_result, expected_context) -> None:\n"
+                "def test_mock_function(param1, param2, param3, param4, expected_result, expected_context) -> None:\n"
                 "    with expected_context:\n"
-                "        assert test_function(param1, param2, param3, param4) == expected_result\n\n\n"
+                "        assert mock_function(param1, param2, param3, param4) == expected_result\n\n\n"
             ),
             does_not_raise(),
         ),
         (
-            test_function,
+            "get_mock_function",
             False,
             True,
             (
@@ -265,100 +340,119 @@ def test_get_return_annotations(
                 "    [\n"
                 "        (param1, param2, param3, param4, expected_result, expected_context),\n"
                 "    ]\n)\n"
-                "def test_test_function(param1, param2, param3, param4, expected_result, expected_context) -> None:\n"
+                "def test_mock_function(param1, param2, param3, param4, expected_result, expected_context) -> None:\n"
                 "    with expected_context:\n"
-                "        assert test_function(param1, param2, param3, param4) == expected_result\n\n\n"
+                "        assert mock_function(param1, param2, param3, param4) == expected_result\n\n\n"
             ),
             does_not_raise(),
         ),
     ],
 )
 def test_get_test(
+    request,
     get_instance: FunctionInspector,
-    func,
+    fixture_name,
     check_types,
     match,
     expected_result,
     expected_context,
 ) -> None:
     with expected_context:
+        func = request.getfixturevalue(fixture_name)
         get_instance.analyse(func)
         assert get_instance.get_test(check_types, match) == expected_result
 
 
 @pytest.mark.parametrize(
-    "func, expected_result, expected_context",
+    "fixture_name, expected_result, expected_context",
     [
         (
-            test_function,
+            "get_mock_function",
             "    with expected_context:\n"
-            "        assert test_function(param1, param2, param3, param4) == expected_result\n",
+            "        assert mock_function(param1, param2, param3, param4) == expected_result\n",
             does_not_raise(),
         ),
     ],
 )
 def test_get_test_body(
-    get_instance: FunctionInspector, func, expected_result, expected_context
+    request,
+    get_instance: FunctionInspector,
+    fixture_name,
+    expected_result,
+    expected_context,
 ) -> None:
     with expected_context:
+        func = request.getfixturevalue(fixture_name)
         get_instance.analyse(func)
         assert get_instance._get_test_body() == expected_result
 
 
 @pytest.mark.parametrize(
-    "func, expected_result, expected_context",
+    "fixture_name, expected_result, expected_context",
     [
         (
-            test_function,
-            "def test_test_function(param1, param2, param3, param4, expected_result, expected_context) -> None:\n",
+            "get_mock_function",
+            "def test_mock_function(param1, param2, param3, param4, expected_result, expected_context) -> None:\n",
             does_not_raise(),
         ),
     ],
 )
 def test_get_test_sig(
-    get_instance: FunctionInspector, func, expected_result, expected_context
+    request,
+    get_instance: FunctionInspector,
+    fixture_name,
+    expected_result,
+    expected_context,
 ) -> None:
     with expected_context:
+        func = request.getfixturevalue(fixture_name)
         get_instance.analyse(func)
         assert get_instance._get_test_sig() == expected_result
 
 
 @pytest.mark.parametrize(
-    "func, item, expected_result, expected_context",
+    "fixture_name, item, expected_result, expected_context",
     [
-        (test_function, "__test__", "test", does_not_raise()),
-        (test_function, "_test", "test", does_not_raise()),
-        (test_function, "test_", "test", does_not_raise()),
-        (test_function, "__test", "test", does_not_raise()),
-        (test_function, 0, "", pytest.raises(TypeError)),
+        ("get_mock_function", "__test__", "test", does_not_raise()),
+        ("get_mock_function", "_test", "test", does_not_raise()),
+        ("get_mock_function", "test_", "test", does_not_raise()),
+        ("get_mock_function", "__test", "test", does_not_raise()),
+        ("get_mock_function", 0, "", pytest.raises(TypeError)),
     ],
 )
 def test_values_strip_underscores(
+    request,
     get_instance: FunctionInspector,
-    func,
+    fixture_name,
     item,
     expected_result,
     expected_context,
 ) -> None:
     with expected_context:
+        func = request.getfixturevalue(fixture_name)
         get_instance.analyse(func)
         assert get_instance._strip_underscores(item) == expected_result
 
 
 @pytest.mark.parametrize(
-    "func, expected_result, expected_context",
+    "fixture_name, expected_result, expected_context",
     [
         (
-            test_function,
-            "def test_function(param1: float, param2: int, param3: bool, param4: str = 'test') -> float:",
+            "get_mock_function",
+            "def mock_function(param1: float, param2: int, param3: bool, param4: str = 'test') -> float:",
             does_not_raise(),
         ),
     ],
 )
 def test_get_func_sig(
-    get_instance: FunctionInspector, func, expected_result, expected_context
+    request,
+    get_instance: FunctionInspector,
+    fixture_name,
+    expected_result,
+    expected_context,
 ) -> None:
     with expected_context:
+        func = request.getfixturevalue(fixture_name)
         get_instance.analyse(func)
         assert get_instance._get_func_sig() == expected_result
 
@@ -416,20 +510,19 @@ def test_insert_string_at_idx(
 
 
 @pytest.mark.parametrize(
-    "func, add_guards, add_debugs, expected_result, expected_context",
+    "fixture_name, add_guards, add_debugs, expected_result, expected_context",
     [
         (
-            test_function,
+            "get_mock_function",
             True,
             True,
             (
-                "@pytest.fixture\n"
-                "def test_function(param1: float, param2: int, param3: bool, param4: str = 'test') -> float:\n"
+                "def mock_function(param1: float, param2: int, param3: bool, param4: str = 'test') -> float:\n"
                 "    for key, val in locals().items():\n"
                 '        logger.debug(f"{key} = {val}")\n'
                 "    if not all([isinstance(param1, float), isinstance(param2, int), isinstance(param3, bool), isinstance(param4, str)]):\n"
                 "        raise TypeError(\n"
-                '            "test_function expects arg types: [float, int, bool, str], "\n'
+                '            "mock_function expects arg types: [float, int, bool, str], "\n'
                 '            f"received: [{type(param1).__name__}, {type(param2).__name__}, {type(param3).__name__}, {type(param4).__name__}]"\n'
                 "        )\n"
                 "    if param3:\n"
@@ -440,12 +533,11 @@ def test_insert_string_at_idx(
             does_not_raise(),
         ),
         (
-            test_function,
+            "get_mock_function",
             False,
             True,
             (
-                "@pytest.fixture\n"
-                "def test_function(param1: float, param2: int, param3: bool, param4: str = 'test') -> float:\n"
+                "def mock_function(param1: float, param2: int, param3: bool, param4: str = 'test') -> float:\n"
                 "    for key, val in locals().items():\n"
                 '        logger.debug(f"{key} = {val}")\n'
                 "    if param3:\n"
@@ -456,15 +548,14 @@ def test_insert_string_at_idx(
             does_not_raise(),
         ),
         (
-            test_function,
+            "get_mock_function",
             True,
             False,
             (
-                "@pytest.fixture\n"
-                "def test_function(param1: float, param2: int, param3: bool, param4: str = 'test') -> float:\n"
+                "def mock_function(param1: float, param2: int, param3: bool, param4: str = 'test') -> float:\n"
                 "    if not all([isinstance(param1, float), isinstance(param2, int), isinstance(param3, bool), isinstance(param4, str)]):\n"
                 "        raise TypeError(\n"
-                '            "test_function expects arg types: [float, int, bool, str], "\n'
+                '            "mock_function expects arg types: [float, int, bool, str], "\n'
                 '            f"received: [{type(param1).__name__}, {type(param2).__name__}, {type(param3).__name__}, {type(param4).__name__}]"\n'
                 "        )\n"
                 "    if param3:\n"
@@ -477,14 +568,16 @@ def test_insert_string_at_idx(
     ],
 )
 def test_add_boilerplate(
+    request,
     get_instance: FunctionInspector,
-    func,
+    fixture_name,
     add_guards,
     add_debugs,
     expected_result,
     expected_context,
 ) -> None:
     with expected_context:
+        func = request.getfixturevalue(fixture_name)
         get_instance.analyse(func)
         assert (
             get_instance.add_boilerplate(add_guards, add_debugs)
