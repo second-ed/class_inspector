a9a357133683b81946848bc9ba91e15a595271c7
gazorpazorpius <edcussmusic@gmail.com>
2024-06-21 20:04:10 +0100

    cleanup: fix unnecessary pd dependency

--- src/class_inspector/_logger.py
+++ src/class_inspector/_logger.py
@@ -2,10 +2,7 @@ import logging
 import logging.config
 import os
 from pathlib import Path
-from typing import Sequence, TypeVar
-
-import pandas as pd
-from openpyxl import Workbook
+from typing import Sequence, TypeVar, Union
 
 T = TypeVar("T")
 
@@ -20,17 +17,13 @@ def setup_logger(file, idx) -> bool:
     return True
 
 
-def compress_logging_value(item: T) -> T:
+def compress_logging_value(item: T) -> Union[T, str]:
     if isinstance(item, (bool, int, float, str)):
         return item
     if isinstance(item, Sequence):
         if len(item) > 10:
             return f"len({len(item)})"
         return item
-    if isinstance(item, pd.DataFrame):
-        return item.info()
-    if isinstance(item, Workbook):
-        return item.sheetnames
     return item
 
 
