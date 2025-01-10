c4b8beb6debffb4a1ad854262bb3350d19634efd
ed cuss <edcussmusic@gmail.com>
2025-01-10 22:09:42 +0000

    docs: add gh-pages ext

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,12 @@ release = "2024"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.coverage", "sphinx.ext.napoleon"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.coverage",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.githubpages",
+]
 
 templates_path = ["_templates"]
 exclude_patterns = []
